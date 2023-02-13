622679738757efa0d980307556af38cf4da2dbdc
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-02-13 15:44:39 -0500

    fixed db.json

--- src/components/FishCard.js
+++ src/components/FishCard.js
@@ -6,7 +6,6 @@ function FishCard({fish, setFish}) {
       <img src={fish["Species Illustration Photo"].src} alt={fish.name} />
       <h4>{fish["Species Name"]}</h4>
       <br></br>
-      <p><b>Availability</b>{fish.Availability}</p> 
     </li>
   );
 }
