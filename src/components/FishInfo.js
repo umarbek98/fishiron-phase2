9da9ea9093371c9e109483ff07f988f619c86d67
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-02-14 16:57:43 -0500

    completed most of submit form

--- src/components/FishInfo.js
+++ src/components/FishInfo.js
@@ -1,6 +1,4 @@
 import React from "react";
-import FishCard from "./FishList"
-import { useParams } from "react-router-dom";
 
 
 function FishInfo({fishInfo}) {
@@ -9,29 +7,28 @@ return(
    <div>
    <section id = "healthFacts">
       <h1>Health Facts:</h1>
-         <p>Calories: {fishInfo.Calories}</p>
+         <p><b>Calories:</b> {fishInfo.Calories}</p>
          <p>Carbohydrate: {fishInfo.Carbohydrate}</p>
          <p>Cholesterol: {fishInfo.Cholesterol}</p>
-         <p>Fat: {fishInfo.[`Fat, Total`]}</p>
+         <p>Fat: {fishInfo[`Fat, Total`]}</p>
          <p>Protein: {fishInfo.Protein}</p>
          <p>Sodium: {fishInfo.Sodium}</p>
       
    </section>
-   <section id = "inTheWildFacts">
+   {fishInfo.Biology || fishInfo.Habitat || fishInfo.Location ? <section id = "inTheWildFacts">
       <h1>In The Wild:</h1>
          <p>Habitat: {fishInfo.Habitat}</p>
          <p>Location {fishInfo.Location}</p>
          <p>Biology: {fishInfo.Biology}</p>
-         <p>Fat: {fishInfo.[`Fat, Total`]}</p>
-         <p>Physical Description: {fishInfo.[`Physical Description`]}</p>
+         <p>Physical Description: {fishInfo[`Physical Description`]}</p>
       
-   </section>
+   </section> : null}
    <section id = "fishingFacts">
       <h1>Fishing Facts</h1>
-         <p>WAIT: {fishInfo.[`Population Status`]}</p>
-         <p>Fishing Rate: {fishInfo.[`Fishing Rate`]}</p>
-         <p>Habitat Vulnurabilites: {fishInfo.[`Habitat Impacts`]}</p>
-         <p>Harvest Type: {fishInfo.[`Harvest Type`]}</p>
+         <p>WAIT: {fishInfo[`Population Status`]}</p>
+         <p>Fishing Rate: {fishInfo[`Fishing Rate`]}</p>
+         <p>Habitat Vulnurabilites: {fishInfo[`Habitat Impacts`]}</p>
+         <p>Harvest Type: {fishInfo[`Harvest Type`]}</p>
        
       
    </section>
--- src/components/NewFishForm.js
+++ src/components/NewFishForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import FishCard from "./FishCard";
+import uuid from "react-uuid";
+
 function NewFishForm({setFish}) {
   const [formData, setFormData] = useState('')
 
@@ -11,16 +12,25 @@ function NewFishForm({setFish}) {
   function handleFishSubmit(e){
     e.preventDefault()
     const newFishData = {
+      "id": uuid(),
       "Species Name": e.target.name.value,
       "Calories": e.target.calories.value,
       "Protein": e.target.protein.value,
       "Color": e.target.color.value,
+      "Cholesterol": e.target.cholesterol.value,
+      "Carbohydrate": e.target.carbohydrate.value,
+      "Fat, Total": e.target.fat.value,
+      "Sodium": e.target.sodium.value,
+      "Population Status": e.target.population.target,
+      "Fishing Rate": e.target.rate.value,
+      "Habitat Impacts": e.target.impact.value,
+      "Harvest Type": e.target.type.value,
       "Species Illustration Photo":{
         src: e.target.image.value
       }
     }
 
-    fetch('http://localhost:6000/fish', {
+    fetch('http://localhost:6001/fish', {
       method: 'POST',
       headers: {
         'Content-Type' : 'application/json'
@@ -40,13 +50,26 @@ function NewFishForm({setFish}) {
         <input onChange={handleChange} value={formData.color} type="text"  name="color" placeholder="Fish Color" />
         <input onChange={handleChange} value={formData.calories} type="text"  name="calories" placeholder="Fish Calories" />
         <input onChange={handleChange} value={formData.protein} type="text"  name="protein" placeholder="Fish Protein" />
+        <input onChange={handleChange} value={formData.cholesterol} type="text"  name="cholesterol" placeholder="Fish Cholesterol" />
+        <input onChange={handleChange} value={formData.carbohydrate} type="text"  name="carbohydrate" placeholder="Fish Carbohydrate" />
+        <input onChange={handleChange} value={formData.fat} type="text"  name="fat" placeholder="Fish Total Fat" />
+        <input onChange={handleChange} value={formData.sodium} type="text"  name="sodium" placeholder="Fish Soidium" />
         <input onChange={handleChange} value={formData.image} type="text"  name="image" placeholder="Image URL" />
+        <h1>Fish Facts</h1> 
+        <input onChange={handleChange} value={formData.population} type="text"  name="population" placeholder="Fish Population" />
+        <input onChange={handleChange} value={formData.rate} type="text"  name="rate" placeholder="Fishing Rate" />
+        <input onChange={handleChange} value={formData.impact} type="text"  name="impact" placeholder="Fishing Rate" />
+        <input onChange={handleChange} value={formData.type} type="text"  name="type" placeholder="Fishing Rate" />
         <button style={{backgroundColor: 'rgb(104, 240, 222)', color: 'black'}} type="submit">Add Fish</button>
       </form>
       {formData.name ? <h1>Name: {formData.name}</h1> : null}
       {formData.color ? <h2>Color: <b style={{color: `${formData.color}`}}>{formData.color}</b></h2> : null}
       {formData.calories ? <h2>Calories: {formData.calories}</h2>: null}
-      {formData.protein ? <h2>Protien: {formData.protein}</h2> : null }
+      {formData.protein ? <h2>Protien: {formData.protein} g</h2> : null }
+      {formData.cholesterol ? <h2>Cholesterol: {formData.cholesterol} mg</h2>: null}
+      {formData.carbohydrate ? <h2>Carbohydrate: {formData.carbohydrate} g</h2>: null}
+      {formData.fat ? <h2>Fat: {formData.fat} g</h2> : null }
+      {formData.sodium ? <h2>Sodium: {formData.sodium} mg</h2>: null}
       <img src={formData.image}/>
     </div>
   );
